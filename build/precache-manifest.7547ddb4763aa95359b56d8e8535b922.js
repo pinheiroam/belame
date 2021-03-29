23bc498a8d6f3a3ab2e9d95048ea01f0546ecec0
Thales Areias <thales.areias@tecsinapse.com.br>
2021-03-28 23:19:23 -0400

    build

---
+++ build/precache-manifest.7547ddb4763aa95359b56d8e8535b922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536fcf1e048fb808e1301ef00622f0a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec0aa7594619c8ef4fa4",
+    "url": "/static/css/2.16aca965.chunk.css"
+  },
+  {
+    "revision": "ec0aa7594619c8ef4fa4",
+    "url": "/static/js/2.89f93750.chunk.js"
+  },
+  {
+    "revision": "ad20e53ee744b837762983ac85ade3bb",
+    "url": "/static/js/2.89f93750.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1a0f98f26d544dced53",
+    "url": "/static/js/main.0e3c3abd.chunk.js"
+  },
+  {
+    "revision": "afcd72b30d465f5feff8",
+    "url": "/static/js/runtime-main.ce0afaea.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c986017baa724ed3eba75727f2f8852.js"
+  "/precache-manifest.7547ddb4763aa95359b56d8e8535b922.js"
 );
 
 self.addEventListener('message', (event) => {
