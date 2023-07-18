c6676545429153e3bb1523434a6813db59413d7c
pinheiroam <arthur.siufgd10@gmail.com>
2023-07-18 18:08:01 -0300

    adiciona produto coffee liss

--- src/markup/pages/coffeeliss.jsx
+++ src/markup/pages/coffeeliss.jsx
@@ -47,13 +47,14 @@ class Coffeeliss extends Component {
                     data-wow-duration="3s">
                     Coffee Liss<span className="bg-primary"></span>
                   </h2>
-                  {/*<p className="wow fadeInUp" data-wow-delay="3s" data-wow-duration="3s">
-                    O Gloss Anti Frizz Plastic Belame foi especialmente desenvolvida com a máxima
-                    tecnologia para suprir as necessidades dos mais exigentes profissionais da área
-                    da beleza. O produto apresenta uma tecnologia que une Óleo de Coco, Óle de
-                    Babaçu e Lumini System, oferece ao profissional um selamento total das cutículas
-                    e cabelos mais resistentes, macios consideráveis no anti frizz.
-                    </p>*/}
+                  <p className="wow fadeInUp" data-wow-delay="3s" data-wow-duration="3s">
+                  Saúde e beleza caminham juntos na vida. Com esse conceito chega ao mercado a COFFEE LISS da BELAME PROFESSIONAL HAIR. 
+                  Um produto inovador, com tecnologia exclusiva de alinhamento térmico dos fios proporcionando  brilho, 
+                  maciez e movimento natural aos cabelo e eliminando o frizz. Sua fórmula contém PROLISS, 
+                  um componente  altamente eficaz na hidratação dos fios capilares e na obtenção de um brilho 
+                  espelhado e macio. Além disso, a COFFEE LISS proporciona  grande otimização no tempo do 
+                  tratamento. Com um menor número de chapadas, saúde e beleza são experimentadas pelos profissionais e sua clientela.
+                  </p>
                 </div>
               </div>
             </div>
@@ -75,11 +76,6 @@ class Coffeeliss extends Component {
                   Passo a Passo<span className="bg-primary"></span>
                 </h2>
               </div>
-              <div className="section-head text-center title-bx ">
-                <h2 className="box-title m-tb0 blog-number" style={{ color: '#6cc000' }}>
-                  Cabelo Loiro<span className="bg-primary"></span>
-                </h2>
-              </div>
               <div className="row justify-content-center">
                 <div
                   className="col-lg-4 col-md-6 col-sm-6 wow fadeInUp m-b20"
@@ -92,12 +88,14 @@ class Coffeeliss extends Component {
                     }}>
                     <div className="icon-bx-wraper left p-lr20 p-tb30 h-100 blog-bx">
                       <span className="icon-cell icon-xxl">
-                        {/*<img src="./images/passos/ENSABOAR.png"></img>*/}
+                        <img src="./images/passos/APLICACAO.png"></img>
                       </span>
                       <div className="icon-content m-b20 p-t5">
                         <h5 className="blog-number">01</h5>
                         <p>
-                          Não precisa lavar o cabelo. Coloque em recipiente o produto Belame e a quantidade equivalente a ponta de um dedo de máscara matizadora.
+                          Não precisa lavar o cabelo. Coloque em recipiente o produto Belame.
+                          Separe o cabelo em mechas finas e aplique o produto homogeneamente. 
+                          SEMPRE IMPORTANTE DEIXAR A DISTÂNCIA DE 1 CM DO COURO CABELUDO.  
                         </p>
                       </div>
                       {/* <a href="#" className="site-button">Read More</a> */}
@@ -113,13 +111,14 @@ class Coffeeliss extends Component {
                     <div className="icon-bx-wraper left p-lr20 p-tb30 h-100 blog-bx">
                       <div className="text-primary">
                         <span className="icon-cell icon-xxl">
-                          <img src="./images/passos/APLICACAO.png"></img>
+                          <img src="./images/passos/TOCA.png"></img>
                         </span>
                       </div>
                       <div className="icon-content m-b20 p-t5">
                         <h5 className=" blog-number">02</h5>
                         <p>
-                          Aplique o produto a partir de 1cm da raiz e deixe agir por 1 hora na touca.
+                          Coloque a touca sem pressionar o cabelo e deixe o produto agir por 30 minutos.
+                          Para cabelos 3c e/ou todas as classificações 4 - deixe agir por 45 minutos a 1 hora.
                         </p>
                       </div>
                       {/* <a href="#" className="site-button">Read More</a> */}
@@ -140,7 +139,9 @@ class Coffeeliss extends Component {
                       </div>
                       <div className="icon-content m-b20 p-t5">
                         <h5 className="blog-number">03</h5>
-                        <p>Enxague o cabelo, removendo 100% do produto.</p>
+                        <p>Após o tempo de ação do produto, enxague o cabelo somente com água, 
+                          removendo 100% do produto.
+                        </p>
                       </div>
                       {/* <a href="#" className="site-button">Read More</a> */}
                     </div>
@@ -161,7 +162,9 @@ class Coffeeliss extends Component {
                       <div className="icon-content m-b20 p-t5">
                         <h5 className="blog-number">04</h5>
                         <p>
-                          Escove bem o cabelo, e pranche de 10 a 15 vezes as mechas em temperatura de 190°C/375°F
+                          Seque o cabelo completamente, sem escovar.
+                          Separe mechas finas e pranche lentamente por 3 a 6 vezes cada mecha.
+                          LEMBRE SEMPRE DE PRANCHAR ADEQUADAMENTE O CABELO, DESDE A RAIZ, SEM FAZER DOBRAS OU MARCAS.
                         </p>
                       </div>
                       {/* <a href="#" className="site-button">Read More</a> */}
@@ -183,123 +186,8 @@ class Coffeeliss extends Component {
                       <div className="icon-content m-b20 p-t5">
                         <h5 className="blog-number">05</h5>
                         <p>
-                          Pode lavar o cabelo e secar para ver o resultado e brilho.
-                        </p>
-                      </div>
-                      {/* <a href="#" className="site-button">Read More</a> */}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="container">
-              <div className="section-head text-center title-bx ">
-                <h2 className="box-title m-tb0 blog-number" style={{ color: '#6cc000' }}>
-                  Cabelo Escuro<span className="bg-primary"></span>
-                </h2>
-              </div>
-              <div className="row justify-content-center">
-                <div
-                  className="col-lg-4 col-md-6 col-sm-6 wow fadeInUp m-b20"
-                  data-wow-delay="0.2s">
-                  <div
-                    className="dlab-box-bg m-b30  fly-box h-100"
-                    style={{
-                      backgroundImage: 'url(' + './images/passos/bg-ensaboar.jpg' + ')',
-                      backgroundColor: '#fff'
-                    }}>
-                    <div className="icon-bx-wraper left p-lr20 p-tb30 h-100 blog-bx">
-                      <span className="icon-cell icon-xxl">
-                        <img src="./images/passos/APLICACAO.png"></img>
-                      </span>
-                      <div className="icon-content m-b20 p-t5">
-                        <h5 className="blog-number">01</h5>
-                        <p>
-                          Não precisa lavar o cabelo, aplique o produto a partir de 1cm da raiz em todo o cabelo.
-                        </p>
-                      </div>
-                      {/* <a href="#" className="site-button">Read More</a> */}
-                    </div>
-                  </div>
-                </div>
-                <div
-                  className="col-lg-4 col-md-6 col-sm-6 wow fadeInUp m-b20"
-                  data-wow-delay="0.4s">
-                  <div
-                    className="dlab-box-bg m-b30  fly-box h-100"
-                    style={{ backgroundImage: 'url(' + './images/passos/bg-secador.jpg' + ')' }}>
-                    <div className="icon-bx-wraper left p-lr20 p-tb30 h-100 blog-bx">
-                      <div className="text-primary">
-                        <span className="icon-cell icon-xxl">
-                          <img src="./images/passos/TOCA.png"></img>
-                        </span>
-                      </div>
-                      <div className="icon-content m-b20 p-t5">
-                        <h5 className="blog-number">02</h5>
-                        <p>Deixar agir por 1 hora na touca.</p>
-                      </div>
-                      {/* <a href="#" className="site-button">Read More</a> */}
-                    </div>
-                  </div>
-                </div>
-                <div
-                  className="col-lg-4 col-md-6 col-sm-6 wow fadeInUp m-b20"
-                  data-wow-delay="0.6s">
-                  <div
-                    className="dlab-box-bg m-b30 fly-box h-100"
-                    style={{ backgroundImage: 'url(' + './images/passos/bg-aplicacao.jpg' + ')' }}>
-                    <div className="icon-bx-wraper left p-lr20 p-tb30 h-100 blog-bx">
-                      <div className="text-primary">
-                        <span className="icon-cell icon-xxl">
-                          <img src="./images/passos/LAVAR.png"></img>
-                        </span>
-                      </div>
-                      <div className="icon-content m-b20 p-t5">
-                        <h5 className=" blog-number">03</h5>
-                        <p>
-                          Enxague 100%. Seque o cabelo 80% com secador, não precisa escovar.
-                        </p>
-                      </div>
-                      {/* <a href="#" className="site-button">Read More</a> */}
-                    </div>
-                  </div>
-                </div>
-                <div
-                  className="col-lg-4 col-md-6 col-sm-6 wow fadeInUp m-b20"
-                  data-wow-delay="0.8s">
-                  <div
-                    className="dlab-box-bg m-b30 fly-box h-100"
-                    style={{ backgroundImage: 'url(' + './images/passos/bg-secador.jpg' + ')' }}>
-                    <div className="icon-bx-wraper left p-lr20 p-tb30 h-100 blog-bx">
-                      <div className="text-primary">
-                        <span className="icon-cell icon-xxl">
-                          <img src="./images/passos/CHAPINHA.png"></img>
-                        </span>
-                      </div>
-                      <div className="icon-content m-b20 p-t5">
-                        <h5 className="blog-number">04</h5>
-                        <p>Pranche de 10 a 15 vezes em mechas finas, de forma lenta e firme.</p>
-                      </div>
-                      {/* <a href="#" className="site-button">Read More</a> */}
-                    </div>
-                  </div>
-                </div>
-                <div
-                  className="col-lg-4 col-md-6 col-sm-6 wow fadeInUp m-b20"
-                  data-wow-delay="0.9s">
-                  <div
-                    className="dlab-box-bg m-b30  fly-box h-100"
-                    style={{ backgroundImage: 'url(' + './images/passos/bg-chapinha.jpg' + ')' }}>
-                    <div className="icon-bx-wraper left p-lr20 p-tb30 h-100 blog-bx">
-                      <div className="text-primary">
-                        <span className="icon-cell icon-xxl">
-                          <img src="./images/passos/ENSABOAR.png"></img>
-                        </span>
-                      </div>
-                      <div className="icon-content m-b20 p-t5">
-                        <h5 className="blog-number">05</h5>
-                        <p>
-                          Pode lavar o cabelo e secar para ver o resultado e brilho.
+                          Lave o cabelo novamente, podendo aplicar a Máscara Pós-química de Extrato de Bambu.
+                          Seque e finalize conforme preferência da cliente.
                         </p>
                       </div>
                       {/* <a href="#" className="site-button">Read More</a> */}
