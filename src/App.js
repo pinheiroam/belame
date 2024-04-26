081312ec3eeffa4e76a2e54342bdb34145ccda9e
Arthur Melo Pinheiro <87482046+pinheiroam@users.noreply.github.com>
2024-04-26 08:05:06 -0300

    whatsapp float button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Markup from './markup/markup';
-
+import { Whatsappbtn } from './markup/element/whatsapp-btn';
 
 
 function App() {
   return (
     <div className="App">
       <Markup />
+      <Whatsappbtn />
     </div>
   );
 }
