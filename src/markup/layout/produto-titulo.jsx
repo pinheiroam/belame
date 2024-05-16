08cea5a3634c0ecf65435790daa75cb35e401313
thales <massafsb@gmail.com>
2024-05-15 21:25:09 -0400

    Component pagina de produtos + imagens + mosaico de produtos

--- src/markup/layout/produto-titulo.jsx
+++ src/markup/layout/produto-titulo.jsx
@@ -14,7 +14,7 @@ class ProdutoTitulo extends Component {
         <div
           className="section-full content-inner bg-white tax-info-chart"
           style={{
-            backgroundImage: 'url(' + './images/background/bg4.png' + ')',
+            backgroundImage: `url(${this.props.backgroundImage})`,
             backgroundPosition: 'top right',
             backgroundRepeat: 'no-repeat'
           }}>
@@ -24,9 +24,7 @@ class ProdutoTitulo extends Component {
                 className="feature-img-bx wow zoomIn col-lg-6 wow fadeInUp text-center"
                 data-wow-delay="1s"
                 data-wow-duration="3s">
-                <div className="video">
-                  {/*<iframe id="ytplayer" type="text/html" src="https://www.youtube.com/embed/FIQGervC0O4?autoplay=1&modestbranding=1&color=white" frameborder="0" allowfullscreen />*/}
-                </div>
+                  <img src={this.props.image} alt={'Imagem do produto ' + this.props.name} />
               </div>
               <div className="section-head text-left title-bx col-lg-6 pt-5 ">
                 <h4
@@ -41,23 +39,19 @@ class ProdutoTitulo extends Component {
                   style={{ color: '#6cc000' }}
                   data-wow-delay="2s"
                   data-wow-duration="3s"
-                > {this.props.name}
-                      <span className="bg-primary"></span>
+                > 
+                {this.props.name} <span className="bg-primary"></span>
                 </h2>
                 <p className="wow fadeInUp" data-wow-delay="3s" data-wow-duration="3s">
-                  Saúde e beleza caminham juntos na vida. Com esse conceito chega ao mercado a COFFEE LISS da BELAME PROFESSIONAL HAIR.
-                  Um produto inovador, com tecnologia exclusiva de alinhamento térmico dos fios proporcionando  brilho,
-                  maciez e movimento natural aos cabelo e eliminando o frizz. Sua fórmula contém PROLISS,
-                  um componente  altamente eficaz na hidratação dos fios capilares e na obtenção de um brilho
-                  espelhado e macio. Além disso, a COFFEE LISS proporciona  grande otimização no tempo do
-                  tratamento. Com um menor número de chapadas, saúde e beleza são experimentadas pelos profissionais e sua clientela.
+                {this.props.text}
                 </p>
               </div>
+              </div>
+              
             </div>
           </div>
         </div>
-      </div>
-        );
+    );
   }
 }
 
---
+++ src/markup/layout/produtos-mosaico.jsx
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+
+
+class ProdutosMosaico extends Component {
+    render() {
+        return (
+            <div className="section-full business-pro-blog" id="nossos-produtos" style={{ backgroundImage: "url(" + './images/background/bg6.png' + ")", backgroundSize: "cover", backgroundPosition: "center" }}>
+                <div className="content-inner business-product">
+                    <div className="container">
+                        <div className="section-head title-bx text-center">
+                            <h2>Nossos Produtos</h2>
+                        </div>
+
+
+                        <div className="row align-items-center d-flex">
+                            <div className="col-md-12 col-lg-3 row align-items-end justify-content-end d-flex">
+                                <div className="d-flex col-lg-10 col-md-6">
+                                    <div className="product-box product-sm wow zoomIn" data-wow-delay="0.6s">
+                                        <div className="product-img">
+                                            <img src="images/product/belame/item5.jpg" alt="" />
+                                        </div>
+                                        <div className="product-details">
+                                            <h4 className="product-title"><a>SOS Capilar</a></h4>
+                                            <span className="product-tage">KIT</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 col-lg-12 product-box wow zoomIn" data-wow-delay="1s">
+                                    <div className="product-details">
+                                        <h4 className="product-title"><a>Plastic Liss Gloss</a></h4>
+                                        <span className="product-tage">UNIDADE</span>
+                                    </div>
+                                    <div className="product-img">
+                                        <img src="images/product/belame/item4.jpg" alt="" />
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                            <div className="col-md-12 col-lg-6">
+                                <div className="row align-items-end d-flex">
+                                    <div className="col-lg-8 col-md-6 col-sm-12">
+                                        <div className="product-box product-lg product-right wow zoomIn" data-wow-delay="0.2s">
+                                            <div className="product-details">
+                                                <h4 className="product-title"><a>Extreme Organic</a></h4>
+                                                <span className="product-tage">UNIDADE</span>
+                                            </div>
+                                            <div className="product-img">
+                                                <img src="images/product/belame/item2.jpg" alt="" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4 col-md-6 col-sm-6">
+                                        <div className="product-box product-sm wow zoomIn" data-wow-delay="0.8s">
+                                            <div className="product-img">
+                                                <img src="images/product/belame/item3.jpg" alt="" />
+                                            </div>
+                                            <div className="product-details">
+                                                <h4 className="product-title"><a>Broto de bambu</a></h4>
+                                                <span className="product-tage">UNIDADE</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4 col-md-6 col-sm-6 d-flex align-self-start">
+                                        <div className="product-box wow zoomIn" data-wow-delay="1s">
+                                            <div className="product-img">
+                                                <img src="images/product/belame/item1.jpg" alt="" />
+                                            </div>
+                                            <div className="product-details">
+                                                <h4 className="product-title"><a>B'TOXX Capilar</a></h4>
+                                                <span className="product-tage">UNIDADE</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-8 col-md-6 col-sm-12">
+                                        <div className="product-box product-lg product-right wow zoomIn" data-wow-delay="0.4s">
+                                            <div className="product-details">
+                                                <h4 className="product-title" style={{ lineHeight: "24px", }}><a>Shampoo Antirresíduos + Gloss Anti Frizz</a></h4>
+                                                <span className="product-tage">KIT</span>
+                                            </div>
+                                            <div className="product-img">
+                                                <img src="images/product/belame/item6.jpg" alt="" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className="col-md-12 col-lg-3">
+                                <div className="product-box wow zoomIn" data-wow-delay="1s">
+                                    <div className="product-details">
+                                        <h4 className="product-title"><a>Plastic Liss Gloss</a></h4>
+                                        <span className="product-tage">UNIDADE</span>
+                                    </div>
+                                    <div className="product-img">
+                                        <img src="images/product/belame/item4.jpg" alt="" />
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-12 col-md-6 col-sm-6">
+                                    <div className="product-box product-sm wow zoomIn" data-wow-delay="0.8s">
+                                        <div className="product-img">
+                                            <img src="images/product/belame/milacro.png" alt="" />
+                                        </div>
+                                        <div className="product-details">
+                                            <h4 className="product-title"><a>Milacro</a></h4>
+                                            <span className="product-tage">UNIDADE</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ProdutosMosaico;
